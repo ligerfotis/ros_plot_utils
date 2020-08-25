10df0c51e7810fb4564f46bead3b418063b335a3
ligerfotis <ligerfotis@gmail.com>
2020-08-25 17:34:34 +0300

    add comments

--- plot_aperture.py
+++ plot_aperture.py
@@ -14,25 +14,27 @@ if not exists(path):
 
 
 def plot_aperture(list_of_files):
+    # load a dictionary of the dataframes of each file
     df_dict = load(list_of_files)
-    # columns = df_dict[list_of_files[0]].columns[1:]
     plt.figure("Aperture", figsize=[12, 8])
-    list_of_handles = []
+
+    # plot a line for each file
     for filename in list_of_files:
         current_df = df_dict[filename]
         timestamps = current_df["TimeStamps"].tolist()
         timestamps = regularize_time(timestamps)
         aperture, blacklist = extractAperture(current_df)
+        # remove the timestamps for which there was a point missing either from the index or thumb fingertips
         [timestamps.pop(idx) for idx in blacklist]
-        list_of_handles.append(plt.plot(timestamps, aperture, label=filename)[0])
+        # plot
+        plt.plot(timestamps, aperture, label=filename)[0]
 
     plt.legend(loc="upper left")
-    # fontP = FontProperties()
-    # plt.legend(handles=list_of_handles, bbox_to_anchor=(1.05, 1), loc='upper left', prop=fontP)
     plt.savefig(path + "aperture")
     # plt.show()
 
 
+# get timestamps relevant to the human action
 def regularize_time(timestamps):
     min_val = min(timestamps)
     timestamps[:] = [val - min_val for val in timestamps]
@@ -40,19 +42,21 @@ def regularize_time(timestamps):
 
 
 def extractAperture(current_df):
-    # calculate aperture
     aperture = []
 
-    index = np.asarray([current_df["RIndex4FingerTip_x"], current_df["RIndex4FingerTip_y"], current_df["RIndex4FingerTip_z"]]).T
-    thumb = np.asarray([current_df["RThumb4FingerTip_x"], current_df["RThumb4FingerTip_y"], current_df["RThumb4FingerTip_z"]]).T
+    index = np.asarray(
+        [current_df["RIndex4FingerTip_x"], current_df["RIndex4FingerTip_y"], current_df["RIndex4FingerTip_z"]]).T
+    thumb = np.asarray(
+        [current_df["RThumb4FingerTip_x"], current_df["RThumb4FingerTip_y"], current_df["RThumb4FingerTip_z"]]).T
     blacklist = []
 
-    for index, (index_point, thumb_point) in enumerate(zip(index, thumb)):
+    for idx, (index_point, thumb_point) in enumerate(zip(index, thumb)):
         tmp_aperture = distance.euclidean(index_point, thumb_point)
-        if tmp_aperture != distance.euclidean(index_point, [0, 0, 0]) or tmp_aperture != distance.euclidean(thumb_point, [0, 0, 0]):
+        if tmp_aperture != distance.euclidean(index_point, [0, 0, 0]) \
+                or tmp_aperture != distance.euclidean(thumb_point, [0, 0, 0]):
             aperture.append(tmp_aperture)
         else:
-            blacklist.append(index)
+            blacklist.append(idx)
 
     return aperture, blacklist
 
